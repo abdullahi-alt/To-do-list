b2c5d4faf3340b1d280cad58186d4e330b8d5b5c
abdullahi=alt <amohakaka@gmail.com>
2025-01-15 20:39:45 +0300

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>welcome to  react js course</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minima. Debitis repellat ullam odit, natus blanditiis minus, dolor rerum cupiditate maxime nobis similique, dolore recusandae aliquid quia accusamus facere consequatur ad omnis obcaecati quos fugiat assumenda velit ab. Excepturi tenetur placeat quae totam aut vel a aliquam perferendis voluptatum minima minus id quia praesentium ratione quasi, omnis reiciendis itaque illo adipisci neque nemo, explicabo autem? Eveniet natus facere animi perspiciatis soluta, rerum optio, odio repellat, minus sequi veniam blanditiis aliquid ipsum cum eligendi architecto eum nobis recusandae nesciunt. Distinctio asperiores maiores nostrum qui enim nemo consectetur reiciendis doloremque aspernatur commodi.</p>
+    </div>
+  )
+}
